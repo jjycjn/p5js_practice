9671869c2e6a076ff9a9f7f1c5c8b16ba45b8d5e
jjycjn <jjycjn@gmail.com>
2017-07-06 15:53:30 -0400

    Add files via upload

---
+++ p5_js_practice_004.js
@@ -0,0 +1,83 @@
+var Start = "F";
+var Rules = [];
+var Result = Start;
+var lineLength = 1000;
+var n = 0;
+
+Rules[0] = {
+  input: "F", 
+  output: "-G+F+G-"
+}
+Rules[1] = {
+  input: "G", 
+  output: "+F-G-F+"
+}
+
+function Generate() {
+  var nextResult = "";
+  for (var i = 0; i < Result.length; i++) {
+    var check = Result.charAt(i);
+    for (var j = 0; j < Rules.length; j++) {
+      var found = false;
+      if (check == Rules[j].input) {
+        nextResult += Rules[j].output;
+        var found = true;
+        break;
+      }
+    }
+    if (!found) {
+      nextResult += check;
+    }
+  }
+  return nextResult;
+}
+
+function Turtle() {
+  for (var i = 0; i < Result.length; i++) {
+    var check = Result.charAt(i);
+    if (check == "F") {
+      line(0, 0, 0, -lineLength);
+      translate(0, -lineLength);
+    } else if (check == "G") {
+      line(0, 0, 0, -lineLength);
+      translate(0, -lineLength);
+    } else if (check == "+") {
+      rotate(+PI/3);
+    } else if (check == "-") {
+      rotate(-PI/3);
+    }
+  }
+}
+
+function setup() {
+  createCanvas(640, 600);
+  background(255);
+  textSize(15);
+  text("n = " + n, 110, 565);
+  var button = createButton("Generate")
+    button.mousePressed(Button);
+  button.position(30, 550);
+}
+
+function draw() {
+}
+
+function Button() {
+  background(255);
+  n += 1;
+  if (n > 9) {
+    background(255);
+    Result = "F";
+    lineLength = 1000;
+    n = 0;
+    text("n = " + n, 110, 565);
+  } else {
+    lineLength /= 2; 
+    text("n = " + n, 110, 565);
+    push();
+    translate(500, 550);
+    Turtle();
+    pop();
+    Result = Generate();
+  }
+}
